9cb835b200899ba7bf01597628720011a614b5e5
Daniel Gosnell <djgosnell@gmail.com>
2014-03-20 20:45:05 +0000

    Started work on class generation.
    git-tfs-id: [https://djgosnell.visualstudio.com/DefaultCollection/]$/DtronixModel;C46

---
+++ DtxModel/DtxModelGen/CommandOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using System.IO;
+
+namespace DtxModelGen {
+	class CommandOptions {
+		[Option("code-outupt", Required = false, HelpText = "The C# file to output the generated code to.")]
+		public string CodeOutput { get; set; }
+
+		[Option("code-type", Required = false, HelpText = "The type of C# code the generate.  Allowed values: DtxModel.")]
+		public string CodeType { get; set; }
+
+		[Option("code-namespace", Required = false, HelpText = "Namespace to place the generated code inside.")]
+		public string CodeNamespace { get; set; }
+
+		[Option("sql-outupt", Required = false)]
+		public string SqlOutput { get; set; }
+
+		[Option("database-output", Required = false)]
+		public string DatabaseOutput { get; set; }
+
+
+		private string _dbml_input;
+
+		[Option("dbml-input", Required = true)]
+		public string DbmlInput {
+			get { return _dbml_input; }
+			set {
+				if (File.Exists(value) == false) {
+					throw new ArgumentException("The dbml-input file does not exist.");
+				}
+				_dbml_input = value;
+			}
+		}
+
+	}
+}
--- DtxModel/DtxModelGen/Program.cs
+++ DtxModel/DtxModelGen/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using DtxModelGen.Schema.Dbml;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace DtxModelGen {
 	class Program {
 		static void Main(string[] args) {
+
+			var options = new CommandOptions();
+			Database database = null;
+
+			var parser = new CommandLine.Parser((CommandLine.ParserSettings settings) => {
+				settings.HelpWriter = Console.Out;
+			});
+			var parse_result = parser.ParseArguments(args, options);
+
+
+			// Verify that the parsing was successful.
+			if (parse_result == false) {
+				writeColor("Invalid input parameters.", ConsoleColor.Red);
+				return;
+			}
+
+			using (FileStream stream = new FileStream(options.DbmlInput, FileMode.Open)) {
+				var serializer = new XmlSerializer(typeof(Database));
+				database = (Database)serializer.Deserialize(stream);
+			}
+
+			if (options.SqlOutput != null) {
+				//var sql_writer = new SqlWriter(database);
+				//sql_writer.WriteTo(options.SqlOutput);
+			}
+
+		}
+
+
+		public static void writeColor(string text, ConsoleColor color) {
+			Console.ForegroundColor = color;
+			Console.Write(text);
+		}
+
+		public static void writeLineColor(string text, ConsoleColor color) {
+			writeColor(text, color);
+			Console.WriteLine();
 		}
 	}
 }
