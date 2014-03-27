0e47a3007244d7aa6351af8eea5dc10f4f6a4665
Daniel Gosnell <djgosnell@gmail.com>
2014-03-27 19:11:09 +0000

    Started on table creator in DtxModeler
    git-tfs-id: [https://djgosnell.visualstudio.com/DefaultCollection/]$/DtronixModel;C55

--- DtxModel/DtxModeler/Form1.Designer.cs
+++ DtxModel/DtxModeler/Form1.Designer.cs
@@ -23,12 +23,32 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.tableItem1 = new DtxModeler.Forms.Controls.TableItem();
+			this.SuspendLayout();
+			// 
+			// tableItem1
+			// 
+			this.tableItem1.Location = new System.Drawing.Point(270, 97);
+			this.tableItem1.Name = "tableItem1";
+			this.tableItem1.Size = new System.Drawing.Size(252, 304);
+			this.tableItem1.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(706, 551);
+			this.Controls.Add(this.tableItem1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private Forms.Controls.TableItem tableItem1;
+
 	}
 }
 
---
+++ DtxModel/DtxModeler/Forms/Controls/TableItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DtxModeler.Forms.Controls {
+	public partial class TableItem : UserControl {
+		public TableItem() {
+			InitializeComponent();
+		}
+
+	}
+}
