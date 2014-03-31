7e1effa0109ebe48843ffab925bfb52949589133
Daniel Gosnell <djgosnell@gmail.com>
2014-03-31 23:23:54 +0000

    Creating ititial window for editing and creating DTML files.
    git-tfs-id: [https://djgosnell.visualstudio.com/DefaultCollection/]$/DtronixModel;C59

--- DtxModel/DtxModeler/Xaml/MainWindow.xaml.cs
+++ DtxModel/DtxModeler/Xaml/MainWindow.xaml.cs
@@ -19,7 +19,9 @@ namespace DtxModeler {
 	/// </summary>
 	public partial class MainWindow : Window {
 		public MainWindow() {
-			InitializeComponent();
+		}
+
+		private void MenuItem_Click(object sender, RoutedEventArgs e) {
 		}
 	}
 }
