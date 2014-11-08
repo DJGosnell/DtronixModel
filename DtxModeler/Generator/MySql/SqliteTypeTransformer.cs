f7232f66b4a82b573c74ba6bf531780de10d4822
Daniel Gosnell <DJGosnell@gmail.com>
2014-11-08 12:36:15 -0500

    Added missing files from last commit.

---
+++ DtxModeler/Generator/MySql/SqliteTypeTransformer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DtxModeler.Ddl;
+
+namespace DtxModeler.Generator.MySql {
+	class MySqlTypeTransformer : TypeTransformer {
+		public MySqlTypeTransformer() {
+			types = new TypeTransformerType[]{
+				new TypeTransformerType(NetTypes.Boolean, "BOOL"),
+				new TypeTransformerType(NetTypes.Boolean, "BOOLEAN"),
+
+				new TypeTransformerType(NetTypes.Int16, "TINYINT"),
+				new TypeTransformerType(NetTypes.Int16, "SMALLINT"),
+
+				new TypeTransformerType(NetTypes.Int32, "INT"),
+				new TypeTransformerType(NetTypes.Int32, "INTEGER"),
+				new TypeTransformerType(NetTypes.Int32, "MEDIUMINT"),
+				
+				new TypeTransformerType(NetTypes.Int64, "BIGINT"),
+
+				new TypeTransformerType(NetTypes.Float, "FLOAT"),
+
+				new TypeTransformerType(NetTypes.Double, "DOUBLE"),
+				new TypeTransformerType(NetTypes.Double, "PRECISION"),
+
+				new TypeTransformerType(NetTypes.Decimal, "REAL"),
+				new TypeTransformerType(NetTypes.Decimal, "DECIMAL"),
+				new TypeTransformerType(NetTypes.Decimal, "NUMERIC"),
+
+				new TypeTransformerType(NetTypes.ByteArray, "BLOB"),
+				new TypeTransformerType(NetTypes.ByteArray, "MEDIMUMBLOB"),
+				new TypeTransformerType(NetTypes.ByteArray, "LONGBLOB"),
+
+				new TypeTransformerType(NetTypes.DateTime, "DATE"),
+				new TypeTransformerType(NetTypes.DateTime, "DATETIME"),
+				new TypeTransformerType(NetTypes.DateTime, "TIME"),
+				new TypeTransformerType(NetTypes.DateTimeOffset, "DATETIME"),
+				
+				new TypeTransformerType(NetTypes.String, "CHAR"),
+				new TypeTransformerType(NetTypes.String, "VARCHAR"),
+
+				new TypeTransformerType(NetTypes.String, "TINYTEXT"),
+				new TypeTransformerType(NetTypes.String, "TEXT"),
+				new TypeTransformerType(NetTypes.String, "MEDIUMTEXT"),
+				new TypeTransformerType(NetTypes.String, "LONGTEXT")
+				
+			};
+		}
+	}
+}
---
+++ DtxModeler/Generator/MySqlMwb/MySqlMwbDdlGenerator.cs
@@ -0,0 +1,117 @@
+﻿using DtxModeler.Ddl;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DtxModeler.Generator.MySql;
+using System.IO.Compression;
+using System.Xml.XPath;
+using System.Xml;
+using System.Threading.Tasks;
+
+namespace DtxModeler.Generator.MySqlMwb {
+	public class MySqlMwbDdlGenerator : DdlGenerator {
+
+		List<Configuration> configurations = new List<Configuration>();
+		private string mwb_file_location;
+
+		public MySqlMwbDdlGenerator(string file_location)
+			: base(new MySqlTypeTransformer()) {
+				mwb_file_location = file_location;
+		}
+
+		public override Database GenerateDdl() {
+			Task.Run(() => {
+				var mwb_xml = new XmlDocument();
+				using (var mwb_archive = ZipFile.OpenRead(mwb_file_location)) {
+					var zip_mwb = mwb_archive.GetEntry("document.mwb.xml");
+
+					if (zip_mwb == null) {
+						throw new FileNotFoundException("Could not find the 'document.mwb.xml' inside the specified MWB file.");
+					}
+
+					using (var stream = zip_mwb.Open()) {
+						mwb_xml.Load(stream);
+					}
+
+				}
+
+				var xml_db_schema = mwb_xml.SelectSingleNode(@"/data
+					/value[@struct-name='workbench.Document']
+					/value[@content-struct-name='workbench.physical.Model']
+					/value[@struct-name='workbench.physical.Model']
+					/value[@struct-name='db.mysql.Catalog']
+					/value[@content-struct-name='db.mysql.Schema']
+					/value[@struct-name='db.mysql.Schema']");
+
+				var xml_tables = xml_db_schema.SelectNodes(@"
+					value[@content-struct-name='db.mysql.Table']
+					/value");
+
+				database.Name = xml_db_schema.SelectSingleNode("value[@key='name']").InnerText;
+				//database.Name = Path.GetFileNameWithoutExtension(connection.DataSource);
+
+				// Get the tables.
+				foreach (XmlNode xml_table in xml_tables) {
+					var name_node = xml_table.SelectSingleNode("value[@key=\"name\"]");
+					var table = new Table() {
+						Name = name_node.InnerText
+					};
+
+					var xml_columns = xml_table.SelectNodes(@"value[@key='columns']/value");
+
+					foreach (XmlNode xml_column in xml_columns) {
+						var column = new Column() {
+							Name = xml_column.SelectSingleNode(@"value[@key='name']").InnerText,
+							Description = xml_column.SelectSingleNode(@"value[@key='comment']").InnerText,
+							DefaultValue = xml_column.SelectSingleNode(@"value[@key='defaultValue']").InnerText,
+						};
+
+						var auto_increment = xml_column.SelectSingleNode(@"value[@key='autoIncrement']").InnerText;
+						column.IsAutoIncrement = (auto_increment == "0" || auto_increment == null)? false : true;
+
+						var nullable = xml_column.SelectSingleNode(@"value[@key='isNotNull']").InnerText;
+						column.Nullable = (nullable == "0" || nullable == null) ? true : false;
+
+						var db_type = xml_column.SelectSingleNode(@"link[@struct-name='db.SimpleDatatype']");
+
+						if (db_type != null) {
+							column.DbType = db_type.InnerText.Replace("com.mysql.rdbms.mysql.datatype.", "").ToUpper();
+
+						} else {
+							db_type = xml_column.SelectSingleNode(@"link[@struct-name='db.UserDatatype']");
+
+							if (db_type != null) {
+								column.DbType = db_type.InnerText.Replace("com.mysql.rdbms.mysql.userdatatype.", "").ToUpper();
+
+							} else {
+								throw new Exception("Unknown data type for column '" + column.Name + "' in table '" + table.Name + "'.");
+							}
+						}
+
+						try {
+							column.NetType = TypeTransformer.DbToNetType(column.DbType);
+						} catch {
+							throw new Exception("Unknown data type for column '" + column.Name + "' in table '" + table.Name + "'.");
+						}
+					
+						column.IsPrimaryKey = column.IsAutoIncrement;
+
+
+						table.Column.Add(column);
+					}
+
+
+
+					database.Table.Add(table);
+				}
+			}).Wait();
+
+			return database;
+
+		}
+	}
+}
