95158d70ff2926c7eed82af51b6adbfd026ea5fc
DJGosnell <DJGosnell@gmail.com>
2015-07-16 15:24:18 -0400

    Added NuGet compatibility to DtronixModel.dll.

--- Tools/NugetPush.bat
+++ Tools/NugetPush.bat
@@ -1,1 +1,2 @@
-..\..\..\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe push *.nupkg
+..\..\..\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe push *0.nupkg
+..\..\..\packages\NuGet.CommandLine.2.8.5\tools\nuget.exe push *.symbols.nupkg
